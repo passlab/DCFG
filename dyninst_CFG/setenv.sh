336bd712bfa1e0f5e80323cd68760da3d7cdac76
Yonghong Yan <yanyh15@gmail.com>
2016-12-09 00:27:52 -0500

    update repo for README file

--- dyninst_CFG/setenv.sh
+++ dyninst_CFG/setenv.sh
@@ -1,6 +1,5 @@
 # Modify the following two line to point to the right location of your installation
 export DYNINST_INSTALL=/home/yan/pintool/dyninst-install
-export LIBDWARF_INSTALL=/home/yan/pintool/dwarf-20161021-install
 
-export LD_LIBRARY_PATH=${DYNINST_INSTALL}/lib:${LIBDWARF_INSTALL}:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${DYNINST_INSTALL}/lib:$LD_LIBRARY_PATH
 export DYNINSTAPI_RT_LIB=${DYNINST_INSTALL}/lib/libdyninstAPI_RT.so
